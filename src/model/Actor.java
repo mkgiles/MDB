627f3587ef8189263f0187ea137fdb0c41b2a19b
Andrew <sn1perandr3w@gmail.com>
2017-11-26 15:50:53 +0000

    Model additions (Currently pursuing getters/setters)

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -1,5 +1,5 @@
 package model;
-
+import java.time.*;
 import structures.DataList;
 import structures.Link;
 import structures.Pair;
@@ -7,6 +7,11 @@ import structures.Pair;
 public class Actor {
 	private static int uuid = 0;
 	private int uid;
+	private LocalDate dob;
+	private int gender;
+	private String nationality;
+	
+	
 	public Actor() {
 		// TODO Auto-generated constructor stub
 	}
@@ -19,6 +24,16 @@ public class Actor {
 	public void viewActor()
 	{
 		System.out.print("Showing actors");
+	}
+	
+	public void setDob(int yearNum, int monthNum, int dayNum)
+	{
+		dob = LocalDate.of(yearNum, Month.of(monthNum), dayNum);
+	}
+	
+	public void getDob()
+	{
+		System.out.println("Date of Birth: " + dob );
 	}
 	
 }
--- src/model/Movie.java
+++ src/model/Movie.java
@@ -7,6 +7,12 @@ import structures.Pair;
 public class Movie {
 	private static int uuid = 0;
 	private int uid;
+	private String title;
+	private int runningTime;
+	private String genre;
+	private String description;
+	private String posterURL;
+	
 	public Movie() {
 		// TODO Auto-generated constructor stub
 	}
