6552c4de912cbe5c30c094ab067fd73427a06ca6
20075908 <20075908@IT120PC26.wit.ie>
2017-11-29 14:05:20 +0000

    Button handler minimized

--- src/view/Main.java
+++ src/view/Main.java
@@ -9,6 +9,7 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import model.*;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 
@@ -40,24 +41,17 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 	
 	@Override
 	public void handle(ActionEvent event) {
-            System.out.println("Test");     
+            
+			System.out.println("Button pressed " + ((Button) event.getSource()).getId());
+			String buttonPressed = ((Button) event.getSource()).getId();
+			System.out.println(buttonPressed);
+			
+            if(buttonPressed.equals("viewMovies"))
+            {
+            	System.out.println("Let's watch some movies");
+            }
+{
+	}
 	}
 	
-	public void handleViewMovie(ActionEvent event) {
-        System.out.println("View Movies");     
-}
-	
-	public void handleViewActor(ActionEvent event) {
-    System.out.println("View Actors");     
-}
-	
-	//This is how movies are added? Requires Movie be public.
-	public void handleAddMovie(ActionEvent event) {
-        System.out.println("Add Movies");
-       // Movie movie = new Movie("Title of Movie", 1, "Horror", "A Horror movie", "URL of Poster");
-       //API.addMovie(movie);  
-}
-	public void handleAddActors(ActionEvent event) {
-        System.out.println("Add Actors");    
-	}
 }
